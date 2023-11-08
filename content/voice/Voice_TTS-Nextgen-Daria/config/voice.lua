a0929a7d6abeb518f5a5cc2aa229c0aaa0e5a1e7
Atanas Kovachki <crashdeburn@gmail.com>
2023-11-08 09:05:48 +0200

    Update voice.lua

--- content/voice/Voice_TTS-Nextgen-Daria/config/voice.lua
+++ content/voice/Voice_TTS-Nextgen-Daria/config/voice.lua
@@ -1,12 +1,12 @@
 module("VOICE", package.seeall)
 
-function init()
-	dofile(L"%app%/config/config_numbers.lua")					-- Tavolsag string tabla
-	dofile(L"%app%/config/config_condfun.lua")					-- Rules feltetel fuggvenytablak
-	init_functions()
-	dofile(L"%app%/config/config_database.lua")					-- Osszerendelese a ruleokank, conditionoknek es a patterneknek
-	dofile(L"%app%/config/config_transform_tables.lua")			-- Transzformacio tablak
-	dofile(L"%app%/config/config_transforms.lua")				-- Transzformacio kod
-end
-
 engine = L"engine_1_1"
+
+function init()
+	dofile(L"%app%/config/config_numbers.lua")
+	dofile(L"%app%/config/config_condfun.lua")
+	init_functions()
+	dofile(L"%app%/config/config_database.lua")
+	dofile(L"%app%/config/config_transform_tables.lua")
+	dofile(L"%app%/config/config_transforms.lua")
+end
